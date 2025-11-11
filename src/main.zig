6c436107f6d011f93e7d721f2b89dde59544a7e0
gnasc <gabrielnsdev@gmail.com>
2025-11-10 21:18:54 -0300

    stdin input

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,13 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+var buffer: [100]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&buffer);
+const stdin = &stdin_reader.interface;
+
 pub fn main() !void {
     // TODO: Uncomment the code below to pass the first stage
     try stdout.print("$ ", .{});
+    const command = try stdin.takeDelimiter('\n');
+    try stdout.print("{s}: command not found", .{command.?});
 }
