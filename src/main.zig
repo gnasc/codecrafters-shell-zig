d88e21d8211fadd528ee1f420e9698130b3e763f
gnasc <gabrielnsdev@gmail.com>
2025-11-10 22:16:15 -0300

    exit command

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const Commands = enum{
+    exit,
+    not_found,
+};
+
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
@@ -8,10 +13,22 @@ var stdin_reader = std.fs.File.stdin().readerStreaming(&buffer);
 const stdin = &stdin_reader.interface;
 
 pub fn main() !void {
-    // TODO: Uncomment the code below to pass the first stage
     while(true) {
         try stdout.print("$ ", .{});
-        const command = try stdin.takeDelimiter('\n');
-        try stdout.print("{s}: command not found\n", .{command.?});
+        const input = try stdin.takeDelimiter('\n');
+
+        var it = std.mem.tokenizeAny(u8, input.?, " \n");
+        const str_command = it.next().?;
+        const command = std.meta.stringToEnum(Commands, str_command) orelse .not_found;
+
+        switch(command) {
+            .exit => blk: {
+                const str_code = it.peek().?;
+                const code = try std.fmt.parseUnsigned(u8, str_code, 10);
+                break :blk std.process.exit(code);
+            },
+            else => try stdout.print("{s}: command not found\n", .{str_command}),
+        }
+
     }
 }
