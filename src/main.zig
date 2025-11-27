2e623c7d6528168bd7aad1d8ed16038e1c6fd526
gnasc <gabrielnsdev@gmail.com>
2025-11-26 22:37:39 -0300

    figuring out Command/Statement desing

--- src/main.zig
+++ src/main.zig
@@ -1,93 +1,52 @@
 const std = @import("std");
 
 const Command = union(enum) {
-    exit: u8,
+    exit: ?[]const u8,
     echo: []const u8,
     type: []const u8,
-    external: [][]const u8,
 
-    fn initFromString(text: []const u8, args: [][]const u8) !Command {
-        const CommandTagType = @typeInfo(Command).@"union".tag_type.?;
-        const tag = std.meta.stringToEnum(CommandTagType, text).?;
-
-        std.debug.print("{s}", .{args[0]});
-        const command = switch(tag) {
-            .exit => Command{ .exit = try std.fmt.parseUnsigned(u8, args[0], 10) },
-            .echo => Command{ .echo = args[0] },
-            .type => Command{ .type = args[0] },
-            .external => Command{ .external = args },
-        };
-
-        return command;
+    fn execute(self: Command, args: []const u8, writer: *std.Io.Writer) void {
+        switch(self) {
+            .exit => shellExit(args),
+            .echo => shellEcho(args, writer),
+            .type => shellType(args, writer),
+        }
     }
 };
 
 const Statement = union(enum) {
-    command: Command,
-    none: []u8,
-};
+    builtin: Command,
+    invalid: []const u8,
+    empty: void,
 
-const Command2 = union(enum) {
-    exit: u8,
-    echo: []const u8,
-    type: []const u8,
-    not_found: []const []const u8,
+    fn initFromString(argv: []const u8) !Statement {
+        const delimiters = " ";
+        var iterator = std.mem.tokenizeAny(u8, argv, delimiters);
+        const first_arg = iterator.next() orelse {
+            return Statement{ .empty = {} };
+        };
 
-    const map = createMap();
+        const CommandTagType = @typeInfo(Command).@"union".tag_type.?;
+        const tag = std.meta.stringToEnum(CommandTagType, first_arg) orelse {
+            return Statement{ .invalid = first_arg };
+        };
 
-    fn createMap() std.StaticStringMap(Command) {
-        const ssm = std.StaticStringMap(Command);
-
-        const field_map = ssm.initComptime(blk: {
-            const fields = @typeInfo(Command).@"union".fields;
-            var result: [fields.len]struct { []const u8, Command } = undefined;
-
-            inline for (fields, 0..) |field, i| {
-                const command_init = switch (field.type) {
-                    u8 => @unionInit(Command, field.name, 0),
-                    []const u8 => @unionInit(Command, field.name, ""),
-                    []const []const u8 => @unionInit(Command, field.name, undefined),
-                    else => @unionInit(Command, field.name, void{}),
-                };
-
-                result[i] = .{ field.name, command_init };
-            }
-
-            const final = result;
-            break :blk final;
-        });
-
-        return field_map;
+        return Statement{ .builtin = command };
     }
 };
 
-fn parseStdin(input: []const u8, delimiters: []const u8) !Command {
-    var iterator = std.mem.tokenizeAny(u8, input, delimiters);
-    const first = iterator.next().?;
-    var command = Command.map.get(first) orelse @unionInit(Command, "not_found", undefined);
+fn shellExit(args: ?[]const u8) !void {
+    var code = 0;
+    if(args) |a| code = try std.fmt.parseUnsigned(a);
 
-    if (iterator.peek()) |token| {
-        switch (command) {
-            .exit => |*value| value.* = try std.fmt.parseUnsigned(u8, token, 10),
-            .echo, .type => |*value| value.* = iterator.rest(),
-            .not_found => {
-                var argv: [][]const u8 = undefined;
-                var i: usize = 0;
-
-                while(iterator.next()) |arg| : (i += 1) {
-                    argv[i] = arg;
-                    std.debug.print("{s}", .{arg});
-                }
-
-                return @unionInit(Command, "not_found", argv);
-            },
-        }
-    }
-
-    return command;
+    std.process.exit(code);
 }
 
-fn shellSearch(allocator: std.mem.Allocator, paths: []const u8, file: []const u8) !?[]u8 {
+fn shellEcho(args: []const u8, writer: *std.Io.Writer) !void {
+    try writer.print("{s}", .{args});
+}
+
+fn shellSearchExec(allocator: std.mem.Allocator, paths: []const u8, name: []const u8) !?[]u8 {
     const delimiter = std.fs.path.delimiter;
     var path_iterator = std.mem.tokenizeAny(u8, paths, &.{delimiter});
 
@@ -98,7 +57,7 @@ fn shellSearch(allocator: std.mem.Allocator, paths: []const u8, file: []const u8
         var dir_iterator = dir.iterate();
         while (try dir_iterator.next()) |entry| {
             const stat = try dir.statFile(entry.name);
-            if (std.mem.eql(u8, entry.name, file) and stat.mode & 0o111 != 0) {
+            if (std.mem.eql(u8, entry.name, name) and stat.mode & 0o111 != 0) {
                 const full_path = try std.fs.path.join(allocator, &[_][]const u8{ path, entry.name });
 
                 return full_path;
@@ -107,6 +66,34 @@ fn shellSearch(allocator: std.mem.Allocator, paths: []const u8, file: []const u8
     }
 
     return null;
+}
+
+fn shellType(args: []const u8, writer: *std.Io.Writer) !void {
+    const stmt = Statement.initFromString(args);
+    
+    switch(stmt) {
+        .builtin => try writer.print("{s} is a shell builtin"),
+        .empty => try writer.print("no argument provided"),
+        .invalid => |command| {
+            var gpa = std.heap.DebugAllocator(.{}){};
+            defer {
+                const status = gpa.deinit();
+                std.debug.assert(status == .ok);
+            }
+
+            const allocator = gpa.allocator();
+            const PATH_ENV = std.process.getEnvVarOwned(allocator, "PATH");
+            
+            const maybe_path = try shellSearchExec(allocator, PATH_ENV, command);
+
+            if (maybe_path) |path| {
+                defer allocator.free(path.?);
+                try writer.print("{s} is {s}\n", .{ command, path });
+            } else {
+                try writer.print("{s}: not found\n", .{command});
+            }
+        }
+    }
 }
 
 fn shellExec(argv: []const []const u8, writer: *std.Io.Writer) !void {
@@ -123,7 +110,7 @@ fn shellExec(argv: []const []const u8, writer: *std.Io.Writer) !void {
 
     const PATH_ENV = env_map.get("PATH").?;
 
-    const path = try shellSearch(allocator, PATH_ENV, argv[0]);
+    const path = try shellSearchExec(allocator, PATH_ENV, argv[0]);
     
     if(path) |p| {
         argv[0] = p;
@@ -138,36 +125,6 @@ fn shellExec(argv: []const []const u8, writer: *std.Io.Writer) !void {
 
 }
 
-fn shellType(args: []const u8, writer: *std.Io.Writer) !void {
-    const command = try parseStdin(args, " \n");
-
-    switch (command) {
-        .not_found => |value| {
-            var gpa = std.heap.DebugAllocator(.{}){};
-            defer {
-                const status = gpa.deinit();
-                std.debug.assert(status == .ok);
-            }
-
-            const allocator = gpa.allocator();
-
-            var env_map = try std.process.getEnvMap(allocator);
-            defer env_map.deinit();
-
-            const PATH_ENV = env_map.get("PATH").?;
-            const path = try shellSearch(allocator, PATH_ENV, value[0]);
-
-            if (path) |p| {
-                defer allocator.free(path.?);
-                try writer.print("{s} is {s}\n", .{ value[0], p });
-            } else {
-                try writer.print("{s}: not found\n", .{value[0]});
-            }
-        },
-        else => try writer.print("{s} is a shell builtin\n", .{@tagName(command)}),
-    }
-}
-
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
@@ -176,21 +133,17 @@ var stdin_reader = std.fs.File.stdin().readerStreaming(&buffer);
 const stdin = &stdin_reader.interface;
 
 pub fn main() !void {
-    var aaa: [2][]const u8 = .{"0", "1"};
-    _ = try Command.initFromString("exit", &aaa);
-    aaa = .{"0", "1"};
     
-//    std.debug.print("{any}", .{cmd});
-//    while (true) {
-//        try stdout.print("$ ", .{});
-//        const input = try stdin.takeDelimiter('\n');
-//        const command = try parseStdin(input.?, " \n");
+    while (true) {
+        try stdout.print("$ ", .{});
+        const input = try stdin.takeDelimiter('\n');
+        const stmt = try Statement.initFromString(input.?);
+        std.debug.print("{any}", .{stmt});
 
-//      switch (command) {
-//            .exit => |value| std.process.exit(value),
-//            .echo => |value| try stdout.print("{s}\n", .{value}),
-//            .type => |value| try shellType(value, stdout),
-//            .not_found => |value| try stdout.print("{s}: command not found\n", .{value[0]}),
-//        }
-//    }
+        switch (stmt) {
+            .builtin => |command| command.execute(stdout),
+            .invalid => |name| try stdout.print("{s}: command not found\n", .{name}),
+            .empty => {}
+        }
+    }
 }
