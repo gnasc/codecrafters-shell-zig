782bc1ffd15bc8144bf80e842bae92b21f2bc643
gnasc <gabrielnsdev@gmail.com>
2025-11-28 21:16:21 -0300

    exec command: works, but the entire solution feels wrong and hard to read... needs more refactoring

--- src/main.zig
+++ src/main.zig
@@ -6,36 +6,53 @@ const CommandKind = enum{ builtin, external, invalid, empty };
 const Statement = struct {
     tag: ?CommandTag,
     kind: CommandKind,
-    args: []const u8,
+    args: union{ single: []const u8, multiple: [][]const u8 },
 
-    fn initFromString(args: []const u8) !Statement {
+    fn initFromString(allocator: std.mem.Allocator, args: []const u8) !Statement {
         const delimiters = " ";
         var iterator = std.mem.tokenizeAny(u8, args, delimiters);
 
         const first_arg = iterator.next() orelse {
-            return Statement{ .tag = null, .kind = .empty, .args = "" };
+            return Statement{ .tag = null, .kind = .empty, .args = .{ .single = "" } };
         };
 
         const tag = std.meta.stringToEnum(CommandTag, first_arg) orelse {
-            return Statement{ .tag = null, .kind = .invalid, .args = first_arg };
+            const PATH_ENV = try std.process.getEnvVarOwned(allocator, "PATH");
+            defer allocator.free(PATH_ENV);
+
+            const maybe_path = try shellSearchExec(allocator, PATH_ENV, first_arg);
+
+            if(maybe_path) |path| {
+                var arg_array = try std.ArrayList([]const u8).initCapacity(allocator, 64);
+
+                try arg_array.append(allocator, path);
+
+                while(iterator.next()) |token| {
+                    try arg_array.append(allocator, token);
+                }
+
+                const extern_args = try arg_array.toOwnedSlice(allocator);
+
+                return Statement{ .tag = null, .kind = .external, .args = .{ .multiple = extern_args } };        
+            }
+
+            return Statement{ .tag = null, .kind = .invalid, .args = .{ .single = first_arg } };
         };
 
-        return Statement{ .tag = tag, .kind = .builtin, .args = iterator.rest() };
+        return Statement{ .tag = tag, .kind = .builtin, .args = .{ .single = iterator.rest() } };
     }
-
-    fn execute(self: Statement, writer: *std.Io.Writer) !void {
+    
+    fn execute(self: Statement, allocator: std.mem.Allocator, writer: *std.Io.Writer) !void {
         switch(self.kind) {
             .builtin => switch(self.tag.?) {
-                .exit => shellExit(self.args),
-                .echo => try shellEcho(self.args, writer),
-                .type => try shellType(self.args, writer),
+                .exit => shellExit(self.args.single),
+                .echo => try shellEcho(self.args.single, writer),
+                .type => try shellType(allocator, self.args.single, writer),
             },
-            .external => {},
-            .invalid => try writer.print("{s}: command not found\n", .{self.args}),
+            .external => try shellExec(allocator, self.args.multiple, writer),
+            .invalid => try writer.print("{s}: command not found\n", .{self.args.single}),
             .empty => {},
         }
-
-
     }
 };
 
@@ -70,63 +87,36 @@ fn shellSearchExec(allocator: std.mem.Allocator, paths: []const u8, name: []cons
     return null;
 }
 
-fn shellType(args: []const u8, writer: *std.Io.Writer) !void {
-    const stmt = try Statement.initFromString(args);
+fn shellType(allocator: std.mem.Allocator, args: []const u8, writer: *std.Io.Writer) !void {
+    const stmt = try Statement.initFromString(allocator, args);
     
     switch(stmt.kind) {
         .builtin => try writer.print("{s} is a shell builtin\n", .{@tagName(stmt.tag.?)}),
         .empty => try writer.print("no argument provided\n", .{}),
         .invalid => {
-            var gpa = std.heap.DebugAllocator(.{}){};
-            defer {
-                const status = gpa.deinit();
-                std.debug.assert(status == .ok);
-            }
-
-            const allocator = gpa.allocator();
             const PATH_ENV = try std.process.getEnvVarOwned(allocator, "PATH");
             defer allocator.free(PATH_ENV);
 
-            const maybe_path = try shellSearchExec(allocator, PATH_ENV, stmt.args);
+            const maybe_path = try shellSearchExec(allocator, PATH_ENV, stmt.args.single);
 
             if (maybe_path) |path| {
                 defer allocator.free(path);
-                try writer.print("{s} is {s}\n", .{ stmt.args, path });
+                try writer.print("{s} is {s}\n", .{ stmt.args.single, path });
             } else {
-                try writer.print("{s}: not found\n", .{stmt.args});
+                try writer.print("{s}: not found\n", .{stmt.args.single});
             }
         },
         else => {},
     }
 }
 
-fn shellExec(argv: []const []const u8, writer: *std.Io.Writer) !void {
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer {
-        const status = gpa.deinit();
-        std.debug.assert(status == .ok);
-    }
-
-    const allocator = gpa.allocator();
-
-    var env_map = try std.process.getEnvMap(allocator);
-    defer env_map.deinit();
-
-    const PATH_ENV = env_map.get("PATH").?;
-
-    const path = try shellSearchExec(allocator, PATH_ENV, argv[0]);
-    
-    if(path) |p| {
-        argv[0] = p;
-        const result = std.process.Child.run(.{
-            .allocator = allocator,
-            .argv = argv,
-        });
-        defer result.deinit();
+fn shellExec(allocator: std.mem.Allocator, args: [][]const u8, writer: *std.Io.Writer) !void {
+    const result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = args,
+    });
         
-        try writer.print("{}", .{result.stdout});
-    }
-
+    try writer.print("{s}", .{result.stdout});
 }
 
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
@@ -137,11 +127,18 @@ var stdin_reader = std.fs.File.stdin().readerStreaming(&buffer);
 const stdin = &stdin_reader.interface;
 
 pub fn main() !void {
-    
+    var gpa = std.heap.DebugAllocator(.{}){};
+    defer {
+        const status = gpa.deinit();
+        std.debug.assert(status == .ok);
+    }
+
+    const allocator = gpa.allocator();
+
     while (true) {
         try stdout.print("$ ", .{});
         const input = try stdin.takeDelimiter('\n');
-        const stmt = try Statement.initFromString(input.?);
-        try stmt.execute(stdout);
+        const stmt = try Statement.initFromString(allocator, input.?);
+        try stmt.execute(allocator, stdout);
     }
 }
